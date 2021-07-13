369677b7efd4c5878919d099b077a9c37036eae0
farahriz <41633573+farahriz@users.noreply.github.com>
2021-07-13 23:24:42 +0800

    Paste in an example sudoku problem board

--- printgrid.py
+++ printgrid.py
@@ -1,6 +1,18 @@
+board = [
+    [7,8,0,4,0,0,1,2,0],
+    [6,0,0,0,7,5,0,0,9],
+    [0,0,0,6,0,1,0,7,8],
+    [0,0,7,0,4,0,2,6,0],
+    [0,0,1,0,5,0,9,3,0],
+    [9,0,4,0,6,0,0,0,5],
+    [0,7,0,3,0,0,0,1,2],
+    [1,2,0,0,0,7,4,0,0],
+    [0,4,9,2,0,6,0,0,7]
+]
+
 class Grid:
   def __init__(self):
-    self.board = [
+    self.empty_board = [
       [0,0,0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0,0,0],
@@ -11,12 +23,28 @@ class Grid:
       [0,0,0,0,0,0,0,0,0],
       [0,0,0,0,0,0,0,0,0]
     ]
+    self.board = board
   
   def print(self):
     for row in self.board:
       print(row)
     print()
   
+  def print_nice_grid(self):
+    0 0 0 | 0 0 0 | 0 0 0
+    0 0 0 | 0 0 0 | 0 0 0
+    0 0 0 | 0 0 0 | 0 0 0
+    - - -   - - -   - - -
+    0 0 0 | 0 0 0 | 0 0 0
+    0 0 0 | 0 0 0 | 0 0 0
+    0 0 0 | 0 0 0 | 0 0 0
+    - - -   - - -   - - -
+    0 0 0 | 0 0 0 | 0 0 0
+    0 0 0 | 0 0 0 | 0 0 0
+    0 0 0 | 0 0 0 | 0 0 0
+
+    print(neat_grid)
+
   def set_row(self, row_index, new_row):
     ind = row_index - 1
     self.board[ind] = new_row
@@ -30,3 +58,6 @@ grid = Grid()
 grid.print()
 grid.set_column(7, [1,2,3,4,5,6,7,8,9])
 grid.print()
+
+
+1. p
